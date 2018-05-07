e97d96537dcc6c5085ca75c359b5a877edf69044
vsparrow <khan@vsparrow.com>
2018-05-07 11:32:38 -0400

    Show winnner name

--- src/GameResultss.js
+++ src/GameResultss.js
@@ -6,6 +6,26 @@ class GameResultss extends React.Component {
     console.log("winner? " + win);
   }
 
+  results=()=>{
+    // let results = this.props.judgeGame.winner.map((k,v)=>
+    //   <li>{k} has a {v}</li>
+    // )
+    let players = Object.keys(this.props.judgeGame.winner)
+    // return players
+    console.log(players);
+    let winner = null
+    if(this.props.judgeGame[players[0]]){winner = players[0]}
+    else{winner = players[1]}
+    return (
+      <div>
+      <li>Players are {players}</li>
+      <li>WINNER IS {winner}!!!!!!</li>
+      <li>{players[0]} has a hand of {this.props.judgeGame.ranks[players[0]]} </li>
+      <li>{players[1]} has a hand of {this.props.judgeGame.ranks[players[1]]}</li>
+      </div>
+    )
+  }
+
   render(){
     // console.log("judgeGame");
     console.log(this.props.judgeGame);
@@ -14,6 +34,8 @@ class GameResultss extends React.Component {
       <div className="GameResultss">
 
         GameResults are here
+        <h3>Results:</h3>
+        {this.results()}
 
       </div>
     )//return
